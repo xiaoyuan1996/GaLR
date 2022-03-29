3309d4e8e97af5e184e7fcce71ca4f0144686963
yuanzhiqiang <yuanzhiqiang@zhihu.com>
2022-03-29 14:39:44 +0800

    v

--- exec/RSITMD/run_GaLR_rsitmd.sh
+++ exec/RSITMD/run_GaLR_rsitmd.sh
@@ -1,6 +1,6 @@
 cd ../..
 CARD=1
 
-CUDA_VISIBLE_DEVICES=$CARD python train.py --path_opt option/RSITMD_mca/RSICD_GaLR.yaml
+CUDA_VISIBLE_DEVICES=$CARD python train.py --path_opt option/RSITMD_mca/RSITMD_GaLR.yaml
 
-CUDA_VISIBLE_DEVICES=$CARD python test_ave.py --path_opt option/RSITMD_mca/RSICD_GaLR.yaml
+CUDA_VISIBLE_DEVICES=$CARD python test_ave.py --path_opt option/RSITMD_mca/RSITMD_GaLR.yaml
